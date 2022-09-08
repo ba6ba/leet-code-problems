0d32a77c1c83384c0aa993abe1e7636084f9b142
Abdul Basit <malikbasit12345@gmail.com>
2022-09-08 21:11:00 +0500

    added re order data in log files problem

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,5 +1,11 @@
+import `medium-problems`.ReorderDataInLogFiles
+
 fun main(args: Array<String>) {
     val addTwoNumbers = AddTwoNumbers()
     val resultNode = addTwoNumbers.add(MockData.getListNode1(), MockData.getListNode2())
     println(resultNode)
+
+    val reorderDataInLogFiles = ReorderDataInLogFiles()
+    val result = reorderDataInLogFiles.reorderLogFiles(ReorderDataInLogFiles.getArray())
+    println(result)
 }
